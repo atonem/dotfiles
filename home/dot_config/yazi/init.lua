8341ced4415f90efbf78bbce0a8b3dfe6dd80e69
Anton Emmerik <anton.emmerik@leovegas.com>
2025-05-02 17:56:24 +0700

    chore(config): add initial config files

---
+++ home/dot_config/yazi/init.lua
@@ -0,0 +1,28 @@
+-- plugins
+require("full-border"):setup()
+require("git"):setup()
+
+-- HEADER
+-- add host@user in header
+Header:children_add(function()
+	if ya.target_family() ~= "unix" then
+		return ""
+	end
+	return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
+end, 500, Header.LEFT)
+
+-- STATUS
+-- show user/group
+Status:children_add(function()
+	local h = cx.active.current.hovered
+	if h == nil or ya.target_family() ~= "unix" then
+		return ""
+	end
+
+	return ui.Line {
+		ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
+		":",
+		ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
+		" ",
+	}
+end, 500, Status.RIGHT)
